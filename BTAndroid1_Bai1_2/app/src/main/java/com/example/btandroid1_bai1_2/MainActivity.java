bf14897ea22d45519e8760d424bd56416acfcb00
CYN <hienlam748@gmail.com>
2022-04-11 22:45:37 +0700

    Bài tập Android 1-2

---
+++ BTAndroid1_Bai1_2/app/src/main/java/com/example/btandroid1_bai1_2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.btandroid1_bai1_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
